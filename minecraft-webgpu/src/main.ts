453fef0e47dd3dc5e96530c7e7d2bf25e8a789c4
Hypercube8 <146479533+Hypercube8@users.noreply.github.com>
2025-05-23 01:56:28 +0000

    Antialiased triangle

--- minecraft-webgpu/src/main.ts
+++ minecraft-webgpu/src/main.ts
@@ -1,12 +1,8 @@
-import { concentricCircleShader } from "../public/shaders/concentric.ts";
+import hardcodedTriangleShader from "/shaders/hardcoded_triangle.wgsl?raw";
 
 async function main(): Promise<void> {
   const adapter: GPUAdapter | null = await navigator.gpu?.requestAdapter();
-
-  const hasBGRA8unormStorage: boolean | undefined = adapter?.features.has("bgra8unorm-storage");
-  const device: GPUDevice | undefined = await adapter?.requestDevice({
-    requiredFeatures: hasBGRA8unormStorage ? ["bgra8unorm-storage"] : []
-  });
+  const device: GPUDevice | undefined = await adapter?.requestDevice();
 
   if (!device) {
     throw new Error("WebGPU is not supported in this browser.");
@@ -15,46 +11,72 @@ async function main(): Promise<void> {
   const canvas: HTMLCanvasElement | null = document.querySelector('canvas');
   const context: GPUCanvasContext | null = canvas!.getContext('webgpu');
 
-  const presentationFormat = hasBGRA8unormStorage ?
-    navigator.gpu.getPreferredCanvasFormat() :
-    "rgba8unorm";
+  const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
   context!.configure({
     device,
     format: presentationFormat,
-    usage: GPUTextureUsage.TEXTURE_BINDING | 
-           GPUTextureUsage.STORAGE_BINDING
   });
 
   const module: GPUShaderModule = device.createShaderModule({
     label: 'textured cube shader',
-    code: concentricCircleShader(presentationFormat)
+    code: hardcodedTriangleShader
   });
 
-  const pipeline: GPUComputePipeline = device!.createComputePipeline({
-    label: "circles in storage texture",
+  const pipeline: GPURenderPipeline = device!.createRenderPipeline({
+    label: "hardcoded triangle pipeline",
     layout: "auto",
-    compute: {
+    vertex: {
       module
+    },
+    fragment: {
+      module,
+      targets: [{ format: presentationFormat }]
+    },
+    multisample: {
+      count: 4
     }
   });
 
+  const renderPassDescriptor: GPURenderPassDescriptor = {
+    label: "our render pass descriptor",
+    colorAttachments: [
+      {
+        view: context!.getCurrentTexture().createView(),
+        clearValue: [0.3, 0.3, 0.3, 1],
+        loadOp: "clear",
+        storeOp: "store"
+      }
+    ]
+  } 
 
+  let multisampleTexture: GPUTexture;
 
   function render() {
     const canvasTexture: GPUTexture = context!.getCurrentTexture();
 
-    const bindGroup = device!.createBindGroup({
-      layout: pipeline.getBindGroupLayout(0),
-      entries: [
-        { binding: 0, resource: canvasTexture.createView() }
-      ]
-    });
+    if (!multisampleTexture ||
+        multisampleTexture.width !== canvasTexture.width ||
+        multisampleTexture.height !== canvasTexture.height
+    ) {
+      if (multisampleTexture) {
+        multisampleTexture.destroy();
+      }
+
+      multisampleTexture = device!.createTexture({
+        format: canvasTexture.format,
+        usage: GPUTextureUsage.RENDER_ATTACHMENT,
+        size: [canvasTexture.width, canvasTexture.height],
+        sampleCount: 4
+      });
+    }
+
+    (renderPassDescriptor as any).colorAttachments[0].view = multisampleTexture.createView();
+    (renderPassDescriptor as any).colorAttachments[0].resolveTarget = canvasTexture.createView();
 
     const encoder: GPUCommandEncoder = device!.createCommandEncoder({ label: "our encoder" });
-    const pass: GPUComputePassEncoder = encoder.beginComputePass();
+    const pass: GPURenderPassEncoder = encoder.beginRenderPass(renderPassDescriptor);
     pass.setPipeline(pipeline);
-    pass.setBindGroup(0, bindGroup);
-    pass.dispatchWorkgroups(canvasTexture.width, canvasTexture.height);
+    pass.draw(3);
     pass.end();
 
     const commandBuffer: GPUCommandBuffer = encoder.finish();
